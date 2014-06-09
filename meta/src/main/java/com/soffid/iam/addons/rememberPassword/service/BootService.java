19ad71b9db0e5d25831ab61fc5f6129174ff735a
Gabriel Buades <gbuades@soffid.com>
2014-06-09 12:25:15 +0200

    IAM-601 Add syncserver bootstraop service

---
+++ meta/src/main/java/com/soffid/iam/addons/rememberPassword/service/BootService.java
@@ -0,0 +1,10 @@
+package com.soffid.iam.addons.rememberPassword.service;
+
+import com.soffid.mda.annotation.Service;
+
+import es.caib.seycon.ng.servei.ApplicationBootService;
+
+@Service(internal=true)
+public class BootService extends ApplicationBootService {
+
+}
