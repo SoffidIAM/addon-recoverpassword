a650f54214aa2cd857bbe95cba2b9f243ae8cf84
Gabriel Buades <gbuades@soffid.com>
2015-04-15 14:02:00 +0200

    [LOG-106] Beautify error message

--- core/src/main/java/com/soffid/iam/addons/rememberPassword/service/RememberPasswordUserServiceImpl.java
+++ core/src/main/java/com/soffid/iam/addons/rememberPassword/service/RememberPasswordUserServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+import com.soffid.iam.addons.rememberPassword.common.MissconfiguredRecoverException;
 import com.soffid.iam.addons.rememberPassword.common.RecoverMethodEnum;
 import com.soffid.iam.addons.rememberPassword.common.RememberPassConfig;
 import com.soffid.iam.addons.rememberPassword.common.RememberPasswordChallenge;
@@ -402,7 +403,7 @@ public class RememberPasswordUserServiceImpl extends
 		challenge.setMethod(guessRecoveryMethod (user));
 		
 		if (challenge.getMethod() == null)
-			throw new InternalErrorException(
+			throw new MissconfiguredRecoverException(
 					Messages.getString("RememberPasswordUserServiceImpl.UserQuestionsError")); //$NON-NLS-1$
 
 		int request;
---
+++ meta/src/main/java/com/soffid/iam/addons/rememberPassword/common/MissconfiguredRecoverException.java
@@ -0,0 +1,5 @@
+package com.soffid.iam.addons.rememberPassword.common;
+
+public class MissconfiguredRecoverException extends Exception {
+
+}
